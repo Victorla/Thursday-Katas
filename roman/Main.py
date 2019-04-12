00619286fb35810c67a800851b2ecfe55c1ecd35
Victor La <vla@paraport.com>
2019-04-11 23:11:40 -0700

    Updated per spec

--- roman/Main.py
+++ roman/Main.py
@@ -7,9 +7,9 @@ providedDate = input("Please provide a date to be convered into a Roman Numeral:
 validate(providedDate)
 
 # Get Year | Month | Day
-year = datetime.strptime(providedDate , '%Y/%m/%d').year
-month = datetime.strptime(providedDate , '%Y/%m/%d').month
-day = datetime.strptime(providedDate , '%Y/%m/%d').day
+year = datetime.strptime(providedDate , '%m/%d/%Y').year
+month = datetime.strptime(providedDate , '%m/%d/%Y').month
+day = datetime.strptime(providedDate , '%m/%d/%Y').day
 
 #Convert to Roman Numeral 
 RomanYear = numbertoRoman(year)
@@ -17,6 +17,6 @@ RomanMonth = numbertoRoman(month)
 RomanDay = numbertoRoman(day)
 
 #Put back together and print 
-RomanDate = RomanYear+ "/" + RomanMonth + "/" + RomanDay
+RomanDate = RomanMonth + "/" + RomanDay + "/" +  RomanYear
 print (RomanDate)
 
