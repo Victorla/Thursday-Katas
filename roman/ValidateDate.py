c041fdd1c2e1b3a9b531e08573335684d17859c8
Victor La <vla@paraport.com>
2019-04-11 23:12:21 -0700

    more spec updates

--- roman/ValidateDate.py
+++ roman/ValidateDate.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def validate(date_text):
     try:
-        datetime.strptime(date_text, '%Y/%m/%d')
+        datetime.strptime(date_text, '%m/%d/%Y')
     except ValueError:
-        raise ValueError("Incorrect data format, should be YYYY/MM/DD")
+        raise ValueError("Incorrect data format, should be MM/DD/YYYY")
